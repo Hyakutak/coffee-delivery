531af5bf938cd79d659f3a1104f597848127d09c
Pedro Cruz <santahelia.pedro@gmail.com>
2023-02-26 12:31:51 -0300

    Add header in total aplication and create layout default

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,13 +2,16 @@ import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Checkout } from "./pages/Checkout";
 import { CheckoutSuccess } from "./pages/CheckoutSuccess";
+import { DefaultLayout } from "./layouts/DefaultLayout";
 
 export function Router() {
     return (
         <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/checkout" element={<Checkout />} />
-            <Route path="/checkout/checkoutsuccess" element={<CheckoutSuccess />} />
+            <Route path="/" element={<DefaultLayout />}>
+                <Route path="/" element={<Home />} />
+                <Route path="/checkout" element={<Checkout />} />
+                <Route path="/checkout/checkoutsuccess" element={<CheckoutSuccess />} />
+            </Route>
         </Routes>
     );
 }
