53911ae44b2b01782be1dfbd97c8ff52180a3cba
Pedro Cruz <54217967+Hyakutak@users.noreply.github.com>
2023-03-07 17:28:56 -0300

    Create products json

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,18 @@
+import { useState, useEffect } from "react";
 import { Banner } from "../components/Banner";
 
 export function Home() {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch('./products.json', {
+            headers: {
+                Accept: "application/json"
+            }
+        }).then(res => res.json())
+          .then(res => setProducts(res.data))
+    }, []);
+
     return (
         <main>
             <Banner />
