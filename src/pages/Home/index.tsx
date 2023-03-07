3976caa732cdd0c9f2882846d6b61f3b8dda3ce0
Pedro Cruz <54217967+Hyakutak@users.noreply.github.com>
2023-03-07 18:22:19 -0300

    modified past pages

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Banner } from "../components/Banner";
+import { Banner } from "../../components/Banner";
 
 export function Home() {
     const [products, setProducts] = useState([]);
